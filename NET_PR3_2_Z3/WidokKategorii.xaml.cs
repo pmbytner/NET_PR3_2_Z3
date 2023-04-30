da95e6b38f57f7845be99841af8478484ee907cc
PMBytner <pawel.m.bytner@gmail.com>
2023-04-30 13:30:41 +0200

    reorganizacja danych xml - zgrupowanie produktów pod kategoriami, okno widoku kategorii

--- NET_PR3_2_Z3/WidokKategorii.xaml.cs
+++ NET_PR3_2_Z3/WidokKategorii.xaml.cs
@@ -12,15 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace NET_PR3_2_Z3;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class WidokKategorii : Window
 {
-	public MainWindow()
+	public WidokKategorii(XmlElement kategoria)
 	{
+		DataContext = kategoria;
 		InitializeComponent();
 	}
 }
---
+++ NET_PR3_2_Z3/WidokListyKategorii.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace NET_PR3_2_Z3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WidokListyKategorii.xaml
+    /// </summary>
+    public partial class WidokListyKategorii : Window
+    {
+        ListBox listaKategorii;
+        public WidokListyKategorii()
+        {
+            InitializeComponent();
+            listaKategorii = (ListBox)FindName("ListaKategorii");
+        }
+
+		private void OtwórzKategorię(object sender, RoutedEventArgs e)
+		{
+            XmlElement wybranaKategoria = (XmlElement)(listaKategorii.SelectedItem);
+            if(wybranaKategoria != null)
+			    new WidokKategorii(
+				    wybranaKategoria
+				    )
+                    .Show()
+                    ; 
+		}
+	}
+}
